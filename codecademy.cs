8c4ebf43718ecbf0df04e71ec80babe3d220e02a
Janalisa Waugh <iamjanalisa@gmail.com>
2023-06-20 10:35:24 -0500

    Finished array documentation exercise

--- codecademy.cs
+++ codecademy.cs
@@ -11,7 +11,14 @@ namespace DocumentationHunt
       summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
       
       int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
-      
+      string [] summerStrutCopy = new string[8];
+      Array.Copy(summerStrut, summerStrutCopy, 8);
+      Console.WriteLine($"first song of summerStrut copy is {summerStrutCopy[0]}");
+      Array.Reverse(summerStrut);
+      Console.WriteLine($"first song and last song of summerStrut are {summerStrut[0]} and {summerStrut[7]}");
+      Array.Clear(ratings, 0, 8);
+      Console.WriteLine(ratings[0]);
+
       
 
     }

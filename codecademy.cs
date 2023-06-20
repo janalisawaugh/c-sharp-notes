bbae0d1c25e51d63a74d4664d7f03f15f1c80629
Janalisa Waugh <iamjanalisa@gmail.com>
2023-06-20 10:15:34 -0500

    Added array doc exercise

---
+++ codecademy.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DocumentationHunt
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {     
+      string[] summerStrut;
+      
+      summerStrut = new string[] { "Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles" };
+      
+      int[] ratings = { 5, 4, 4, 3, 3, 5, 5, 4 };
+      
+      
+
+    }
+  }
+}

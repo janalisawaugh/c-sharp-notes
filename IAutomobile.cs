6367e22610c2b31c10185c9a787d5833090bff6a
Janalisa Waugh <iamjanalisa@gmail.com>
2023-06-23 13:59:31 -0500

    Changed Sedan to IAutomobile

---
+++ IAutomobile.cs
@@ -0,0 +1,6 @@
+/*
+The interface is a set of properties, methods, and other members. 
+They are declared with a signature but their behaviors are not defined. 
+A class implements an interface if it defines those properties, methods, 
+and other members.
+*/

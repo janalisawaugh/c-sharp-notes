1c83d58fb521d59b05dc327fb87d97bd19c6285b
Janalisa Waugh <iamjanalisa@gmail.com>
2023-06-23 14:06:28 -0500

    Implemented IAutomobile interface in Sedan

--- IAutomobile.cs
+++ IAutomobile.cs
@@ -4,3 +4,19 @@ They are declared with a signature but their behaviors are not defined.
 A class implements an interface if it defines those properties, methods, 
 and other members.
 */
+using System;
+
+namespace LearnInterfaces
+{
+  interface IAutomobile
+  {
+    string LicensePlate
+    {get;}
+    double Speed
+    {get;}
+    int Wheels
+    {get;}
+    void Honk();
+  }
+
+}

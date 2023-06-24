b35a46656ff08d074c29ac0bd1917b9941521171
Janalisa Waugh <iamjanalisa@gmail.com>
2023-06-24 18:51:28 -0500

    Added Truck to the fam

--- Sedan.cs
+++ Sedan.cs
@@ -4,13 +4,42 @@ namespace LearnInterfaces
 {
   class Sedan : IAutomobile
   {
-    public string LicensePlate { get; }
-    public double Speed { get; }
-    public int Wheels { get; }
+
+    public Sedan(double speed)
+    {
+      Speed = speed;
+      LicensePlate = Tools.GenerateLicensePlate();
+      Wheels = 4;
+    }
+
+
+  	public string LicensePlate
+    { get; }
+
+    public double Speed
+    { 
+      get {return Speed;} 
+      private set {Speed = value;}
+    }
+
+    public int Wheels
+    { get; }
+    
     public void Honk()
     {
-      Console.WriteLine("HONK SEDAN");
+      Console.WriteLine("HONK!");
     }
-  }
 
+    public void SpeedUp()
+    {
+      Speed +=5;
+    }
+
+    public void SlowDown()
+    {
+      Speed-=5;
+    }
+
+    
+  }
 }
